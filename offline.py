e4e56e03a85f13f08ac9eacd0e402970260ccfd5
Rajat Mehta <rajatmehta1992@gmail.com>
2019-12-13 19:22:03 +0100

    added comments in print()

--- offline.py
+++ offline.py
@@ -5,27 +5,27 @@ from PIL import Image
 from feature_extractor import FeatureExtractor
 
 fe = FeatureExtractor()
-
-for img_path in sorted(glob.glob('static/img/*.jpg')):
-    print(img_path)
-    img = Image.open(img_path)  # PIL image
-    feature = fe.extract(img)
-    feature_path = 'static/feature/' + os.path.splitext(os.path.basename(img_path))[0] + '.pkl'
-    pickle.dump(feature, open(feature_path, 'wb'))
-
-"""
+print("Extracting feature vectors from gallery images.")
 f=[]
 i=1
 for img_path in sorted(glob.glob('static/img/*.jpg')):
-    
     img = Image.open(img_path)  # PIL image
     feature = fe.extract(img)
     #feature_path = 'static/feature/' + os.path.splitext(os.path.basename(img_path))[0] + '.pkl'
     f.append(feature)
     if i %100 ==0:
         print(i)
-        break
     i+=1
+
 print(len(f))
-pickle.dump(f, open('static/feature/f.pkl', 'wb'))
+pickle.dump(f, open('static/feature/features.pkl', 'wb'))
+print("Feature extraction finished.")
+
+"""
+for img_path in sorted(glob.glob('static/img/*.jpg')):
+    print(img_path)
+    img = Image.open(img_path)  # PIL image
+    feature = fe.extract(img)
+    feature_path = 'static/feature/' + os.path.splitext(os.path.basename(img_path))[0] + '.pkl'
+    pickle.dump(feature, open(feature_path, 'wb'))
 """
